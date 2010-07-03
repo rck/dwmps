6b256243880c061de78c1bc62e25933e5e24627d
rck <dev.rck@gmail.com>
2010-07-03 21:55:03 +0200

    updated laptop config

--- laptop/config.h
+++ laptop/config.h
@@ -33,6 +33,8 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
 	{ "===",      bstackhoriz },
+	{ "(@)",      spiral },
+	{ "[\\]",     dwindle },
 };
 
 /* key definitions */
@@ -69,6 +71,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_s,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_p,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY,                       XK_d,      setlayout,      {.v = &layouts[6]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
